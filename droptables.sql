37e1f0308330b36ecb86cf6724beacf72a9b6c2e
Charles Langlois <schok53@gmail.com>
2016-12-02 13:20:06 -0500

    Correction droptables.sql

--- droptables.sql
+++ droptables.sql
@@ -1,8 +1,1 @@
-BEGIN
-  --Bye Tables!
-  FOR i IN (SELECT ut.table_name
-              FROM USER_TABLES ut) LOOP
-    EXECUTE IMMEDIATE 'drop table '|| i.table_name ||' CASCADE CONSTRAINTS ';
-  END LOOP;
-
-END;
+select 'drop table '||table_name||' cascade constraints;' from user_tables;
