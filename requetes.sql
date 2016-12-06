c662fd644b0b990d2ffa0f39bdb87c3b9268c99e
Gevrai <gevrai@gmail.com>
2016-12-05 21:27:39 -0500

    Edit of requetes.sql, most are working properly

--- requetes.sql
+++ requetes.sql
@@ -1,9 +1,9 @@
 -- Afficher le nombre de transactions d’achats des 5 dernières semaines ;
-SELECT count(*) FROM transaction
-WHERE (SYSTIMESTAMP - dateEtHeure) < INTERVAL '35' DAY;
+SELECT count(*) as nbTransactions FROM transaction
+WHERE (SYSDATE - dateEtHeure) < INTERVAL '99' DAY;
 
 -- Afficher le nombre de clients utilisateurs de Google ;
-SELECT count(*) FROM client
+SELECT count(*) as nbClients FROM client
 WHERE courriel LIKE '%@gmail.%';
 
 -- Afficher les clients de l'arrondissement “B4” (indiqué par le code postale commençant par “B4”) et qui n'ont pas un compte google ;
@@ -14,7 +14,9 @@ AND courriel NOT LIKE '%@gmail.%';
 
 -- Afficher les noms des sous-catégories de la catégorie Sport ;
 SELECT nom FROM categorie
-WHERE parent = 'Sport';
+WHERE parent = (
+    SELECT noCategorie FROM categorie
+    WHERE nom = 'Sports' or nom = 'Sport');
 
 -- Afficher les emplacements de Montréal qui accueillent plus que 2000 clients.
 SELECT nom FROM emplacement, adresse
@@ -23,13 +25,14 @@ AND ville = 'Montreal'
 AND capacite > 2000;
 
 -- Afficher les informations des évènements qui ont plus de 4 occurrences et dont le prix est supérieur à $150 avant rabais ;
-SELECT * FROM evenement
-WHERE evenementID IN (
-    SELECT DISTINCT evenementID
+SELECT titre FROM evenement
+WHERE noEvenement IN (
+    SELECT DISTINCT noEvenement
     FROM occurence
-    GROUP BY evenementID
-    HAVING COUNT(evenementID) > 4
-    AND prix > 150) 
+    WHERE prix > 150
+    GROUP BY noEvenement
+    HAVING COUNT(noEvenement) > 4
+); 
 
 -- Calculer les économies faites par un client de votre choix (id=5) grâce aux coupons rabais de toutes ses transactions;
 SELECT SUM(cout-montantPaye) FROM transaction
@@ -39,18 +42,20 @@ HAVING noClient = 5;
 -- Afficher les informations des évènements qui ont plus de 4 occurrences en Ontario et dont le prix est inférieur à $150 après le rabais “âge d’or”.
 SELECT * from evenement
 WHERE NoEvenement IN (
-      SELECT DISTINCT noEvenement FROM occurence
-      GROUP BY noEvenement
-      HAVING COUNT(noEvenement) > 4
-      AND    Prix < 150*(SELECT Rabais from coupon where nom = 'age dor')
+    SELECT DISTINCT occurence.noEvenement FROM occurence,emplacement,adresse
+    WHERE occurence.noEmplacement = emplacement.noEmplacement
+    AND emplacement.noAdresse = adresse.noAdresse
+    AND (adresse.province = 'ON' OR adresse.province = 'Ontario')
+    AND occurence.prix < 150*(SELECT Rabais FROM coupon WHERE description = 'age d''or')
+    GROUP BY occurence.noEvenement
+    HAVING COUNT(occurence.noEvenement) > 4
 );
-
 
 -- Calculer et afficher les revenus totaux des emplacements pour tous les évènements triés par ordre décroissant. (PS. Faites attention aux statut des transactions).
 SELECT noEvenement, revenus
 FROM transaction
 GROUP BY noEvenement
-HAVING statut = 'payee' OR statut = 'approuvee'
+HAVING (statut = 'payee' OR statut = 'approuvee')
 ORDER BY SUM(montantPaye) as revenus DESC;
 
 -- Afficher l'arborescence de toutes les catégories et toutes leurs sous-categories.
